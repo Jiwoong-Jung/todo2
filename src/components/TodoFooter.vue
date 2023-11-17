c67a3b5e9535215fea45d2a0927428563dee7b49
Jiwoong-Jung <jjw981119@naver.com>
2023-11-17 14:48:59 +0900

    Initial commit

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,31 @@
+<script>
+  export default {
+    methods: {
+      clearTodo() {
+        // localStorage.clear();
+        this.$emit('removeAll');
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+  </div>
+</template>
+
+<style>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
+  .clearAllBtn {
+    color: #e20303;
+    display: block;
+  }
+</style>
